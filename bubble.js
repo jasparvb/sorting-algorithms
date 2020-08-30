7fdf5a29eaba99abdf4fa1de75ef5bcc6b7de527
von Buelow <jaspar@UMICH.EDU>
2020-08-30 00:15:02 -0400

    Bubble sort

--- bubble.js
+++ bubble.js
@@ -1,4 +1,14 @@
 function bubbleSort(arr) {
+  for(let i = 0; i <= arr.length; i++) {
+    for(let y = 0; y <= arr.length; y++) {
+      if(arr[y] > arr[y+1]) {
+        let swap = arr[y];
+        arr[y] = arr[y+1];
+        arr[y+1] = swap;
+      }
+    }
+  }
+  return arr;
 }
 
 module.exports = bubbleSort;
