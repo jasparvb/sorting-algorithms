0017492b236cd3cb54b1b2b888503a0c6fe4c557
von Buelow <jaspar@UMICH.EDU>
2020-08-30 00:15:39 -0400

    Merge fn

--- merge.js
+++ merge.js
@@ -1,4 +1,27 @@
 function merge(arr1, arr2) {
+  let mergedArr = []
+  let i = 0;
+  let j = 0;
+
+  while(i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      mergedArr.push(arr1[i]);
+      i++;
+    } else {
+      mergedArr.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while(i < arr1.length) {
+    mergedArr.push(arr1[i]);
+    i++;
+  }
+  while(j < arr2.length) {
+    mergedArr.push(arr2[j]);
+    j++;
+  }
+  return mergedArr;
 }
 
 function mergeSort(arr) {
